ed41ff6df306335c8172792e1c29041898e55309
Albert Boehmler <ajboehmler@gmail.com>
2019-02-21 09:25:20 -0500

    Initial notes

---
+++ derp.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import random
+import sqlite3
+from datetime import datetime as dt
+from datetime import timedelta as td
+from matplotlib import pyplot as plt
+import seaborn as sns
+
